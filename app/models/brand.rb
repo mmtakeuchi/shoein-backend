df04b1e1a038b466da2dfa2e90282a48ad8d1b72
mmtakeuchi <mikemtaki@gmail.com>
2020-10-13 17:38:52 -0700

    fixed uniqueness validation

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,5 +2,5 @@ class Brand < ApplicationRecord
     has_many :shoes
 
     validates :name, presence: true
-    validates :name, unqiueness: true
+    validates :name, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+nike = Brand.create(image: "nike-4-logo-png-transparent.png", name: "Nike")
+jordan = Brand.create(image: "air-jordan-logo.png", name: "Air Jordan")
+
+jordan1 = Shoe.create(name: "Jordan 11 Space Jam", image: "jordan_11_space_jam.jpeg", size: 11, condition: "NDS", color: "black and white", brand_id: 2)
+offwhite1 = Shoe.create(name: "Off White Air Force One", image: "https://sneakernews.com/wp-content/uploads/2020/07/off-white-nike-air-force-1-mca-blue-black-sample-2.jpg", size: 11, condition: "NDS", color: "University Blue/Black-White", brand_id: 1)
